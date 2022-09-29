ec77c539c5fc1f50a2b4f15f9788c7def0bbca02
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2022-09-29 09:04:03 +0800

    Added reimbursement personel search
    Signed-off-by: SilentCode1003 <j0s3ph0r3nc10@gmail.com>

--- routes/filling.js
+++ routes/filling.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
 
+const { create } = require('xmlbuilder2');
+var xml2js = require('xml2js');
+var fs = require('fs');
+
+var fillingPath = __dirname + '/data/reimbursement/filling/';
+
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname + '/data/establishment/', filename));
+  fs.readFile(filepath, 'utf8', function (err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('filling', { title: 'Budget Monitoring System' });
 });
 
 module.exports = router;
+
+router.post('/save', function (req, res, next) {
+  try {
+    console.log(req.body);
+    var personel = req.body.personel;
+    var date = req.body.date;
+    var data = req.body.data;
+
+    var targetDir = fillingPath + personel;
+
+    console.log(targetDir);
+
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir);
+    }
+
+    try {
+      let fullFileNameFinal = `${targetDir}/${date}_${personel}.json`;
+      fs.writeFileSync(fullFileNameFinal, data, function (err) {
+        if (err) throw err;
+        res.json({
+          msg: 'error',
+          data: err
+        });
+      });
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+
+    setTimeout(function () {
+      res.json({
+        msg: 'success'
+      })
+    }, 1000);
+
+
+  }
+  catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+});
--- routes/reimbursementsummary.js
+++ routes/reimbursementsummary.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+const { create } = require('xmlbuilder2');
+var xml2js = require('xml2js');
+var fs = require('fs');
+
+var targetPath = __dirname + '/data/reimbursement/filling/';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('reimbursementsummary', { title: 'Budget Monitoring System' });
+router.get('/', function (req, res, next) {
+  res.render('reimbursementsummary', { title: 'Budget Monitoring System', reportTitle: 'REIMBURSEMENT FORM' });
 });
 
 module.exports = router;
+
+router.post('/LoadData', function (req, res, next) {
+  var targetDir = targetPath + `/${req.body.personel}/`;
+
+  console.log(targetDir);
+
+  var data_arr = [];
+  fs.readdir(targetDir, (err, files) => {
+    if (err) {
+      res.json({
+        msg: 'error',
+        data: err
+      });
+    }
+
+    files.forEach(file => {
+      console.log(file);
+      var rawTxt = file.split('.');
+      console.log(rawTxt);
+      var txt = rawTxt[0].split('_');
+      console.log(txt);
+      data_arr.push({
+        'date': txt[0],
+        'personel': txt[1]
+      });
+    });
+  });
+
+  setTimeout(function () {
+    res.json({
+      msg: 'success',
+      data: data_arr
+    });
+  }, 1000);
+});
+
+router.post('/retrieveFile', function (req, res, next) {
+
+});
