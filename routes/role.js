cb1bf687a1afd1442efef80d6f52330da000f0d3
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-06-09 09:09:29 +0800

    Revise Role Module

--- routes/role.js
+++ routes/role.js
@@ -1,71 +1,64 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const helper = require('./repository/customhelper');
-const mysql = require('./repository/budgetdb');
-const dictionary = require('./repository/dictionary');
-
-
+const helper = require("./repository/customhelper");
+const mysql = require("./repository/budgetdb");
+const dictionary = require("./repository/dictionary");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('role', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("role", { title: "Express" });
 });
 
 module.exports = router;
-router.get('/load', (req, res) => {
+router.get("/load", (req, res) => {
   try {
-    let sql = 'select * from master_role_type';
+    let sql = "select * from master_role_type";
 
-    mysql.Select(sql, 'MasterRoleType', (err, result) => {
+    mysql.Select(sql, "MasterRoleType", (err, result) => {
       if (err) {
         return res.json({
-          msg: err
-        })
+          msg: err,
+        });
       }
 
       console.log(result);
-    
+
       res.json({
-        msg: 'success',
-        data: result
-      })
+        msg: "success",
+        data: result,
+      });
     });
   } catch (error) {
     res.json({
-      msg: error
-    })
+      msg: error,
+    });
   }
-})
+});
 
-router.post('/save', (req, res) => {
+router.post("/save", (req, res) => {
   try {
     let rolename = req.body.rolename;
-    let createdby = 'CREATOR';
+    let createdby = "CREATOR";
     let createdate = helper.GetCurrentDatetime();
+    let status = dictionary.GetValue(dictionary.ACT());
     let data = [];
 
     console.log(`${rolename}, ${createdby} ${createdate}`);
 
-    data.push([
-      rolename,
-      createdby,
-      createdate
-    ])
+    data.push([rolename, createdby, createdate, status]);
 
     console.log(data);
-    mysql.InsertTable('master_role_type', data, (err, result) => {
+    mysql.InsertTable("master_role_type", data, (err, result) => {
       if (err) console.error(err);
 
       res.json({
-        msg: 'success'
-      })
+        msg: "success",
+      });
     });
-
   } catch (error) {
     res.json({
-      msg: error
-    })
+      msg: error,
+    });
   }
-
-})
+});
