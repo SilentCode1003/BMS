600764d61c0b9fd69cd27d52515644deb48883d2
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-06-13 16:52:40 +0800

    Added getbalance API

--- routes/budget.js
+++ routes/budget.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
 const helper = require("./repository/customhelper");
 const mysql = require("./repository/budgetdb");
@@ -25,6 +25,26 @@ router.get("/load", (req, res) => {
       }
 
       console.log(result);
+
+      res.json({
+        msg: "success",
+        data: result,
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/getbalance", (req, res) => {
+  try {
+    let employeeid = req.body.employeeid;
+    let sql = `select * from employee_budget where eb_employeeid='${employeeid}'`;
+
+    mysql.Select(sql, "EmployeeBudget", (err, result) => {
+      if (err) console.error("Error: ", err);
 
       res.json({
         msg: "success",
