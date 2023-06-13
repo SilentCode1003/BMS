3ccaedc85041f85eb6326a08ca823c56d43bd231
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-06-13 15:07:08 +0800

    Revise Store Module

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -28,11 +30,13 @@ mysql.CheckConnection();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json({ limit: '25mb' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/store.js
+++ routes/store.js
@@ -1,45 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
-const helper = require('./repository/customhelper');
-const mysql = require('./repository/budgetdb');
-const dictionary = require('./repository/dictionary');
-
-
+const helper = require("./repository/customhelper");
+const mysql = require("./repository/budgetdb");
+const dictionary = require("./repository/dictionary");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('store', { title: process.env._TITLE });
+router.get("/", function (req, res, next) {
+  res.render("store", { title: process.env._TITLE });
 });
 
 module.exports = router;
-router.get('/load', (req, res) => {
+router.get("/load", (req, res) => {
   try {
-    let sql = 'select * from master_store';
+    let sql = "select * from master_store";
 
-    mysql.Select(sql, 'MasterStore', (err, result) => {
+    mysql.Select(sql, "MasterStore", (err, result) => {
       if (err) {
         return res.json({
-          msg: err
-        })
+          msg: err,
+        });
       }
 
       console.log(result);
-    
+
       res.json({
-        msg: 'success',
-        data: result
-      })
+        msg: "success",
+        data: result,
+      });
     });
   } catch (error) {
     res.json({
-      msg: error
-    })
+      msg: error,
+    });
   }
-})
+});
 
-router.post('/save', (req, res) => {
+router.post("/save", (req, res) => {
   try {
     let storecode = req.body.storecode;
     let storename = req.body.storename;
@@ -47,11 +45,9 @@ router.post('/save', (req, res) => {
     let email = req.body.email;
     let contact = req.body.contact;
     let status = dictionary.GetValue(dictionary.ACT());
-    let createdby = 'CREATOR';
+    let createdby = "CREATOR";
     let createddate = helper.GetCurrentDatetime();
     let data = [];
-
-    console.log(`${storecode}, ${storename},  ${address}, ${email}, ${contact}, ${status}, ${createdby} ${createddate}`);
 
     data.push([
       storecode,
@@ -61,22 +57,100 @@ router.post('/save', (req, res) => {
       contact,
       status,
       createdby,
-      createddate
-    ])
+      createddate,
+    ]);
 
     console.log(data);
-    mysql.InsertTable('master_store', data, (err, result) => {
+    mysql.InsertTable("master_store", data, (err, result) => {
       if (err) console.error(err);
 
       res.json({
-        msg: 'success'
-      })
+        msg: "success",
+      });
     });
-
   } catch (error) {
     res.json({
-      msg: error
-    })
+      msg: error,
+    });
   }
+});
 
-})
+router.post("/excelsave", (req, res) => {
+  try {
+    let data = req.body.data;
+    let status = dictionary.GetValue(dictionary.ACT());
+    let createdby = "CREATOR";
+    let createddate = helper.GetCurrentDatetime();
+    data = JSON.parse(data);
+    let counter = 0;
+    let dup_data = "";
+    master_store = [];
+
+    data.forEach((key, item) => {
+      // if (key.contact == null) console.log(`${key.storeno}`);
+
+      let sql_check = `select * from master_store where ms_storecode='${key.storecode}' and ms_storename='${key.storename}'`;
+      let isDuplicate = false;
+
+      mysql
+        .isDataExist(sql_check, "MasterStore")
+        .then((result) => {
+          if (result) {
+            isDuplicate = true;
+            dup_data += `${key.storecode} ${key.storename}'`;
+          } else {
+            master_store.push([
+              key.storecode,
+              key.storename,
+              key.address,
+              key.email,
+              key.contact,
+              status,
+              createdby,
+              createddate,
+            ]);
+          }
+
+          counter += 1;
+
+          if (data.length == counter) {
+            if (master_store.length != 0) {
+              console.log("Insert Data");
+              mysql.InsertTable("master_store", master_store, (err, result) => {
+                if (err) {
+                  console.log(err);
+                  return res.json({
+                    msg: "error",
+                    error: err,
+                  });
+                } else {
+                  if (isDuplicate) {
+                    return res.json({
+                      msg: "duplicate",
+                      data: dup_data,
+                    });
+                  }
+
+                  res.json({
+                    msg: "success",
+                  });
+                }
+              });
+            } else {
+              return res.json({
+                msg: "duplicate",
+                data: dup_data,
+              });
+            }
+          }
+        })
+        .catch((error) => {
+          return res.json({ msg: error });
+        });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
