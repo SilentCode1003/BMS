253d69a267d2fb5e43d6c020852d68396f118e35
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-06-14 11:57:06 +0800

    Added save API on requestbudget module

--- routes/requestbudget.js
+++ routes/requestbudget.js
@@ -32,3 +32,52 @@ router.get("/load", (req, res) => {
     });
   }
 });
+
+router.post("/save", (req, res) => {
+  try {
+    let requestby = req.body.requestby;
+    let requestdate = helper.GetCurrentDate();
+    let transactiondate = helper.GetCurrentDatetime();
+    let budget = req.body.budget;
+    let details = req.body.details;
+    let status = dictionary.GetValue(dictionary.REQB());
+    let request_budget = [];
+    let employee_budget_history = [];
+
+    request_budget.push([requestby, requestdate, budget, details, status]);
+    employee_budget_history.push([
+      requestdate,
+      requestby,
+      budget,
+      status,
+      requestby,
+      transactiondate,
+    ]);
+
+    mysql.InsertTable(
+      "budget_request_details",
+      request_budget,
+      (err, result) => {
+        if (err) console.error("Error: ", err);
+        console.log(result);
+
+        mysql.InsertTable(
+          "employee_budget_history",
+          employee_budget_history,
+          (err, result) => {
+            if (err) console.error("Error: ", err);
+            console.log(result);
+
+            res.json({
+              msg: "success",
+            });
+          }
+        );
+      }
+    );
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
